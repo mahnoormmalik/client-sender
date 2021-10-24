b48dea05b8cdd36272f4b8cdb8395ec5fe571af1
mahnoor malik <mahnoormmalik@gmail.com>
2021-10-24 14:31:05 +0200

    added random ID publisher

---
+++ README.md
@@ -0,0 +1,11 @@
+## Create a venv:
+
+python3 -m venv venv
+
+## Activate venv:
+
+source venv/bin/activate
+
+## Install Requirements:
+
+python3 -m pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import random
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Congratulations, it's a web app!"
+    IDsize = 1000 #Size of ID vector
+    ID = [random.choice([-1,1]) for i in range(IDsize)]
+    # print(ID)
+    # ID = list(map(ID, lambda:str))
+    # print(ID)
+    return " ".join(map(str, ID))
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5002, debug=True)
